1dd3b6505a6859d48211b0991c1cae1cd80ae7d8
Alistair Smith <alistairsmith01@gmail.com>
2020-12-22 22:03:45 +0000

    refactoring: remove this generic im not sure why it was there lol

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,7 +1,4 @@
-export function useReadingTime<T extends HTMLElement>(
-  content: string,
-  wordsPerMinute = 260
-) {
+export function useReadingTime(content: string, wordsPerMinute = 260) {
   const match = content.match(/\w+/g) ?? [];
   return Math.ceil(match.length / wordsPerMinute);
 }
