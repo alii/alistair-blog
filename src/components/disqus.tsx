a36412a2c844e3c958a6b9f4345842af26cfb652
Alistair Smith <alistairsmith01@gmail.com>
2021-01-04 18:38:30 +0000

    fix: disqus shortname

--- src/components/disqus.tsx
+++ src/components/disqus.tsx
@@ -2,7 +2,8 @@ import { DiscussionEmbed } from "disqus-react";
 import { Post } from "../lib/types";
 
 export const DisqusComments = ({ post }: { post: Post }) => {
-  const disqusShortname = "your-disqus-shortname";
+  const disqusShortname = "alistaircloud";
+
   const disqusConfig = {
     url: `https://blog.alistair.cloud/post/${post.slug}`,
     identifier: post.slug,
